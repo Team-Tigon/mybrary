493a93a51f7d16156e8809350acbabeba046aad3
samtalks <sam.ikiru@gmail.com>
2013-11-26 12:17:19 -0500

    first commit to expierement branch

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Item do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should let you set a name for an item" do
+    my_item = Item.new(name: "Rails 4 Way")
+
+    expect(Item.all.find_by(name: "Rails 4 Way").to eq(my_item)
+  end
+
 end
