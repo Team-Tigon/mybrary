3ba0035282e6afa3b757ca229c68645e5c1a69f0
jamesjtong <jtong33@gmail.com>
2013-12-04 17:04:40 -0500

    changes keys back

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '657297557654145', '375c7b6f819b660dae6757f3d42cdb59'
+  provider :facebook, '566428166769731', 'ad23507458195a270fc50643e7ff2f79'
 end
