e377a4f338a5fe96752a21d7e6a7179d5b8365e6
jamesjtong <jtong33@gmail.com>
2013-12-04 10:03:12 -0500

    ss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Mybrary::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+  config.assets.precompile += %w( glyphicons-halflings-regular.eot glyphicons-halflings-regular.svg glyphicons-halflings-regular.ttf glyphicons-halflings-regular.woff )
 
   # Generate digests for assets URLs.
   config.assets.digest = true
